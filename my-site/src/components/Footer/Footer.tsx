df56930328d8c83e3f1ec5efab919e2a68b55a9f
Marcin Nikel <marcin.positive@gmail.com>
2023-11-07 14:09:26 +0100

    added hoover fill for svgs

--- my-site/src/components/Footer/Footer.tsx
+++ my-site/src/components/Footer/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import {Mail, Github, Linkedin, X} from "../../app/icons"
 function Footer() {
   return (
-    <footer>
-      <div>
+    <footer className="w-full bg-background shadow-footer mt-12 py-4 fixed bottom-0">
+      <div className="flex items-center justify-center gap-2">
         <a href={''} aria-label="E-mail">
-          <Mail className="" />
+          <Mail className="contact_icons" />
         </a>
         <a href={''} aria-label="Github">
           <Github className="" />
@@ -16,6 +16,9 @@ function Footer() {
         <a href={''} aria-label="X">
           <X className="" />
         </a>
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-string">All rights reserved by mnikel </p>
       </div>
     </footer>
   );
